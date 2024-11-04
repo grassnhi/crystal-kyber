23a144c3d1d912a5bbfb64f0f3b3608f0f02046a
NPAnh1109 <phucanh11903@gmail.com>
2024-11-04 19:39:20 +0700

    NTT

---
+++ src/ntt_processor.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/17/2024 05:56:47 PM
+// Design Name: 
+// Module Name: ntt_processor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ntt_processor(
+
+    );
+endmodule
